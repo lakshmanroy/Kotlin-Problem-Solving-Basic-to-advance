2a88787a087ec25160148fadea02397f0b5c81a8
Lakshman Roy <lkrd5115@gmail.com>
2024-02-16 19:10:05 +0530

    commit

---
+++ Fundamentals of Kotlin/StringDataTypes.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    var a:String="Lakshman"
+    var b:String="Riya"
+    println(a+ "" +b)
+
+    var x:String= "Android"
+    println("length of size=" +x.length)
+}
