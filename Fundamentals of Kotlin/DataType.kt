8faf073ea9c88681c9e1f54cb10042c7dafb59ad
Lakshman Roy <lkrd5115@gmail.com>
2024-02-16 18:43:34 +0530

    commit

---
+++ Fundamentals of Kotlin/DataType.kt
@@ -0,0 +1,19 @@
+fun main(args: Array<String>) {
+    var a:Boolean=true
+    var b:Char='A'
+    var c:Int=45
+    var d:Float=43.3f
+    var e:Double=54.43
+    var f:Byte=56
+    var g:Short=564
+    var h:Long=-345654334L
+    
+    println(a)
+    println(b)
+    println(c)
+    println(d)
+    println(e)
+    println(f)
+    println(g)
+    println(h)
+}
