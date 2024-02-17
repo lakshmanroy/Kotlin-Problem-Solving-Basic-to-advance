7c91b95524a727000cdd5b2d2a2feed6bd562efd
Lakshman Roy <lkrd5115@gmail.com>
2024-02-17 11:32:44 +0530

    commit

---
+++ Operator in Kotlin/OperatorPrecedence.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+    var x:Int=5+4*3
+    println(x)
+
+    var a:Int=4
+    var b:Int=6
+    var c:Int=8
+    var sum=0
+
+    sum= a+ --b + ++c
+    println(sum)
+}
