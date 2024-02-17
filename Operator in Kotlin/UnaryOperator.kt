97d5380e1537f13c1bc151d4fb07096d010ed109
Lakshman Roy <lkrd5115@gmail.com>
2024-02-17 11:05:13 +0530

    commit

---
+++ Operator in Kotlin/UnaryOperator.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    var number:Int=10
+    var isChecked:Boolean=true
+
+    println("+number " + +number)
+    println("-number " + -number)
+    println("++number " + ++number)
+    println("--number " + --number)
+    println("!isChecked =" + !isChecked)
+}
