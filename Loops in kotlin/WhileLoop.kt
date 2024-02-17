9ebbb2ad3d68e9426657bd25e5aac16327ae19ee
Lakshman Roy <lkrd5115@gmail.com>
2024-02-17 17:46:31 +0530

    commit

---
+++ Loops in kotlin/WhileLoop.kt
@@ -0,0 +1,21 @@
+fun main(args: Array<String>) {
+
+    var n=1
+    var fact=1
+    while(n<=5)
+    {
+        fact*=n
+        println("$n!=$fact")
+        n++
+    }
+    
+
+     /*var i=1
+      while(i<=10){
+        println(i)
+        i++;
+    }
+    */
+
+    
+}
