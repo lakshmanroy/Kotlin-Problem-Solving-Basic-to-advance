5a0440da76cd2e915954d6b63d7323c14aec6ca2
Lakshman Roy <lkrd5115@gmail.com>
2024-02-17 10:45:53 +0530

    commit

---
+++ Operator in Kotlin/ArithmeticOperator.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>) {
+    var number1:Double=8.4
+    var number2:Double=4.2
+    var result:Double
+
+    result=number1+number2
+    println("number1 + number=" +result)
+    result=number1-number2
+    println("number1 - number=" +result)
+    result=number1*number2
+    println("number1 * number=" +result)
+    result=number1/number2
+    println("number1 / number=" +result)
+    result=number1%number2
+    println("number1 % number=" +result)
+}
