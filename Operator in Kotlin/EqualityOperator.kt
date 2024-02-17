35c20e896b02b0c3de4e9c8537a91edabbb78e86
Lakshman Roy <lkrd5115@gmail.com>
2024-02-17 11:12:34 +0530

    commit

---
+++ Operator in Kotlin/EqualityOperator.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+    var a:Int=10
+    var b:Int=5
+
+    println("a==b :" +(a==b))
+    println("a>b :" +(a>b))
+    println("a<b :" +(a<b))
+    println("a>=b :" +(a>=b))
+    println("a<=b :" +(a<=b))
+    println("a!=b :" +(a!=b))
+
+}
