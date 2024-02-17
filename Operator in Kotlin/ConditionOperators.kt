31ff5742e8672640d186e2d639672d1ed4f4bc29
Lakshman Roy <lkrd5115@gmail.com>
2024-02-17 11:25:38 +0530

    commit

---
+++ Operator in Kotlin/ConditionOperators.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>) {
+    var number1:Int=12
+    var number2:Int=10
+    var number3:Int=20
+
+    var result:Boolean=false
+
+    result= (number1>number2 && (number3>number2))
+    println("Result=" +result)
+
+    result= (number1>number2 || (number3>number2))
+    println("Result=" +result)
+
+
+    
+}
