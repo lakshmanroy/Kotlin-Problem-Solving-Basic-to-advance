78ece09971fa0ca241242f0d4f39fb7f3fff1c9d
Lakshman Roy <lkrd5115@gmail.com>
2024-02-18 10:35:20 +0530

    commit

---
+++ Object Oriented Programming (OOP)/SecondaryConstructor.kt
@@ -0,0 +1,16 @@
+class MySecondaryCar {
+    var name:String?=null
+    var model:Int?=null
+      
+    constructor(name:String,model:Int){
+        this.name=name
+        this.model=model
+
+    }
+
+}
+
+fun main(args: Array<String>) {
+    var mySecondCar=MySecondaryCar("Mercedes",2000)
+    println("your car's name is ${mySecondCar.name} and mode is ${mySecondCar.model}")
+}
