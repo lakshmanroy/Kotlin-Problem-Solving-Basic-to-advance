79c4da3e66065407604fe2d3728ad6306be918cc
Lakshman Roy <lkrd5115@gmail.com>
2024-02-18 12:09:33 +0530

    commit

---
+++ Object Oriented Programming (OOP)/Encapsulation.kt
@@ -0,0 +1,17 @@
+class MySecondaryCar {
+    var name:String?=null
+    var model:Int?=null
+     private set
+             get
+      
+    constructor(name:String,model:Int){
+        this.name=name
+        this.model=model
+    }
+
+}
+
+fun main(args: Array<String>) {
+    var mySecondCar=MySecondaryCar("Mercedes",2000)
+    println("your car's name is ${mySecondCar.name} and mode is ${mySecondCar.model}")
+}
